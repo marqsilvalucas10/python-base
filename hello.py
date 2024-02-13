e72ba35501dd5aeab9f0172d17e872705b73e9d6
Lucas <marqsilvalucas10@gmail.com>
2024-02-13 10:37:40 -0300

    Meu segundo fumo

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print('O fim se aproxima.'.upper())
+#!/usr/bin python3
+""" Hello world """
+import os 
+
+current_language = os.getenv("LANG")[:5]
+msg = "Debuqi zon de taibou"
+if current_language == 'pt_BR':
+    msg = "Damiao"
+elif current_language == "it_IT":
+    msg = "mama mia"
+
+print(msg)
